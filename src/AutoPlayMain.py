ab1c68e76e7d7b4e48b68d727409f5cae55ff6b8
Danny Nguyen <danny.h.nguyen94@gmail.com>
2023-08-01 19:26:34 -0700

    Initial File Creation

---
+++ src/AutoPlayMain.py
@@ -0,0 +1,10 @@
+"""
+    Author: Danny Nguyen
+    Created: 08/01/2023
+    Description:
+        The script works like a macro recorder, where it will perform a subset of keyhandles or mouse clicks
+        but will only do so when detecting subset of images on screen.
+"""
+
+if __name__ == "__main__":
+    print("Hello World")
